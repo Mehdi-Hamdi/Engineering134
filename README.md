c64c73c339c4635f100d1e330d47d85c1e5c9d8c
Mehdi-Hamdi <mhamdi@spartaglobal.com>
2022-11-30 16:27:34 +0000

    commit

---
+++ README.md
@@ -0,0 +1,38 @@
+Hello
+
+# Title 
+### subtitle 
+###### smallest subtitle
+
+## Lists
+- 1
+- 2
+- 3 
+
+## Quotes
+>yes
+>>yesss
+>>>yesssss
+
+## Images
+![image](file name)
+
+## Links
+[markdown cheatsheet](https://enterprise.github.com/downloads/en/markdown-cheatsheet.pdf)
+
+-  [Title](#Title)
+
+## Github markdown
+
+'this is a code snippet'
+
+'''c#
+int i = 10 + c
+'''
+
+- [x] done
+- [ ] not done
+
+Name | Street | Job
+---|---|---
+Tim | Legit lane| lion tamer
---
+++ example script - powershell.ps1
@@ -0,0 +1,30 @@
+﻿Get-Command -CommandType Cmdlet *date* > powershell-Date-Commands.txt
+
+echo "This is a my profile batch file"
+# Create a folder if it does not already exist
+if (Get-Item -Path FolderA -ErrorAction Ignore) {
+    echo "Folder already exists"
+}
+else
+{
+
+    echo "Creating folder"
+    md FolderA
+}
+#
+cd FolderA
+# Create a file
+echo "My Profile" > testfile.txt
+# Append Content to the file
+echo "New Line" >> testfile.txt
+echo Another line without quotes >> testfile.txt
+# Display file contents
+echo "File after appending text"
+cat testfile.txt
+# Add content to the file - this overwrites the previous contents
+echo "Mehdi's profile" > testfile.txt
+# Display file contents
+echo "File after adding text"
+cat testfile.txt
+# move back to parent directory
+cd ..
