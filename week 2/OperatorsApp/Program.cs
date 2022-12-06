279223f9487d060cc21c3711153ef2abc79c66b5
Mehdi-Hamdi <mhamdi@spartaglobal.com>
2022-12-06 11:45:51 +0000

    saving operators app

---
+++ week 2/OperatorsApp/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+internal class program
+{
+
+    static void Main(string[] args)
+    {
+        int x = 5;
+        int y = 5;
+        x++;
+        ++y;
+
+        int a = x++;
+        int b = ++y;
+        for (int i = 1; i <= 5; ++i)
+        {
+            Console.WriteLine(i);
+        }
+        var c = 5 / 2;
+        var d = 5.0 / 2;
+        var e = 5 / 3;
+        double f = 5 / 2;
+
+        var daysInAWeek = 7;
+        var totalDaysToBirthday = 23;
+        var weeksToBirthday = totalDaysToBirthday / daysInAWeek;
+        var days = totalDaysToBirthday % daysInAWeek;
+
+        Console.WriteLine(EvenOdd(5));
+    }
+    public static bool EvenOdd(int num)
+    {
+        return num % 2 == 0;
+    }
+    public static int GetStones(int totalPounds) 
+    {
+    return totalPounds / 14; 
+    }
+    public static int GetPounds(int totalPounds)
+    { 
+        return totalPounds % 14;
+    }
+
+}
