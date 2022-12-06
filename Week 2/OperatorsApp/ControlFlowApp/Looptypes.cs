248f10eb0c699d5efe3d2af14af37e921102b05a
Mehdi-Hamdi <mhamdi@spartaglobal.com>
2022-12-06 15:22:41 +0000

    initial unit test lesson commit

---
+++ Week 2/OperatorsApp/ControlFlowApp/Looptypes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlFlowApp
+{
+    public static class Looptypes
+    {
+        internal static int HighestDoWhileLoop(List<int> nums)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static int HighestForEachLoop(List<int> nums)
+        {
+            int highest = nums[0];
+            foreach (int i in nums)
+            {
+                if (i > highest)
+                {
+                    highest = i;
+                }
+            } return highest;
+
+        }
+
+        internal static int HighestForLoop(List<int> nums)
+        {
+            int highest = nums[0];
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] > highest)
+                {
+                    highest = nums[i];
+                }
+            }
+
+            return highest;
+
+            
+        }
+
+        internal static string HighestWhileLoop(List<int> nums)
+        {
+            
+        }
+    }
+}
--- week 2/OperatorsApp/ControlFlowApp/Program.cs
+++ week 2/OperatorsApp/ControlFlowApp/Program.cs
@@ -12,7 +12,15 @@ namespace ControlFlowApp
 
             Console.WriteLine(Priority(1));
             Console.WriteLine(CanDrive(18));
+
+            List<int> nums = new List<int> { 10, 6, 22, -17, 3, };
+            Console.WriteLine("Highest foreach loop: " + Looptypes.HighestForEachLoop(nums));
+            Console.WriteLine("Highest for- loop: " + Looptypes.HighestForLoop(nums));
+            Console.WriteLine("Highest while- loop: " + Looptypes.HighestWhileLoop(nums));
+            Console.WriteLine("Highest do-while loop: " + Looptypes.HighestDoWhileLoop(nums));
         }
+
+        List<int> nums = new List<int> { 10, 6, 22, -17, 3, };  
         public static string Priority(int level)
         {
             string priority = "code";
