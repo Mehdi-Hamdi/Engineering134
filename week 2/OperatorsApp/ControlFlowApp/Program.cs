304c278664db016dfee6561041512b197cc50438
Mehdi-Hamdi <mhamdi@spartaglobal.com>
2022-12-06 13:48:03 +0000

    adding control flow app

---
+++ week 2/OperatorsApp/ControlFlowApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ControlFlowApp
+{
+    internal class program
+    {
+
+        static void Main(string[] args)
+        {
+            int mark = 90;
+            var grade = mark >= 65 ? (mark >= 85 ? "distinction" : "Pass") : "Fail";
+
+            Console.WriteLine(Priority(1));
+            Console.WriteLine(CanDrive(18));
+        }
+        public static string Priority(int level)
+        {
+            string priority = "code";
+            switch (level)
+            {
+                case 3:
+                    priority = priority + " Red";
+                    break;
+                case 2:
+                    priority = priority + " Amber";
+                    break;
+                case 1:
+                    priority = priority + " Green";
+                    break;
+            }
+            return priority;
+        }
+            public static bool CanDrive(int age)
+            {
+                switch (age)
+                {
+                    case >= 17:
+                        return true;
+                        default: return false;
+                }
+            }
+        }
+    }
