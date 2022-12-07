056b89688e5498463829082875e686240a0888e4
Mehdi-Hamdi <mhamdi@spartaglobal.com>
2022-12-07 17:41:43 +0000

    Unit test lab completed

---
+++ Week 2/UnitTestLab/UnitTestLab/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace UnitTestLab
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AvailableClassifications(15));
+        }
+        public static string AvailableClassifications(int ageOfViewer)
+        {
+            string result;
+            if (ageOfViewer < 12)
+            {
+                result = "U & PG films are available.";
+            }
+            else if (ageOfViewer < 15)
+            {
+                result = "U, PG & 12 films are available.";
+            }
+            else if (ageOfViewer <= 17)
+            {
+                result = "U, PG, 12 & 15 films are available.";
+            }
+            else if (ageOfViewer <= 18)
+            {
+                result = "U, PG, 12, 15 & 18 films are available.";
+            }
+            else
+            {
+                result = "All films are available.";
+            }
+            return result;
+        }
+    }
+    
+}
