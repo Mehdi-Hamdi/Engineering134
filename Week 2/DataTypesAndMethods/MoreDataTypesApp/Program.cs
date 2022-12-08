0909949be6e93cb1e6b66a3deca2cda810419e5f
Mehdi-Hamdi <mhamdi@spartaglobal.com>
2022-12-08 16:02:35 +0000

    string builder exercise

---
+++ Week 2/DataTypesAndMethods/MoreDataTypesApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace MoreDataTypesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myString = " C# list fundamentals";
+            Console.WriteLine(StringBuilderExercise(myString));
+        }
+
+        public static string StringBuilderExercise(string myString)
+        {
+            var trimmedUpperString = myString.ToUpper().Trim();
+            var nPos = trimmedUpperString.IndexOf('N');
+            StringBuilder sb = new StringBuilder(trimmedUpperString);
+            sb.Replace('L', '*').Replace('T', '*').Remove(nPos + 1, sb.Length - nPos - 1);
+            return sb.ToString();
+        }
+    }
+}
