7391b18401b65246b9a7441557079ee3ec5acc13
Mehdi-Hamdi <mhamdi@spartaglobal.com>
2022-12-08 15:20:27 +0000

    completed test first  exercies

---
+++ TestFirstStarter/02b_TestFirstExercises/TestFirstExercises_Lib/Methods.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestFirstExercises
+{
+    public class Methods
+    {
+        // implement this method so that it returns true if x is greater than or equal to y
+        public static bool GreaterEqual(int x, int y)
+        {
+            var GreaterEqual = x >=  y ? true : false; return GreaterEqual;
+            
+        }
+
+        // Implement this method so that it take an int as an input,
+        // squares it, adds 101, divides the result by 7, then subtracts 4.  
+        // Return a double rounded to 3 decimal places.
+        public static double BODMAS(int inputNumber)
+        {
+            var input = (((inputNumber * inputNumber) + 101.0)/7.0) - 4;
+            
+            return Math.Round(input, 3);
+        }
+
+        // implement this method so that it returns true if num is even, otherwise false
+        public static bool EvenOdd(int num)
+        {
+            var EvenOdd = num % 2 == 0 ? true : false;
+                return EvenOdd;
+        }
+
+        // implement this method so that it returns 
+        // the sum of all numbers between 1 and n inclusive 
+        // that are divisible by either 2 or 5
+        public static int SumEvenFive(int max)
+        {
+            int sum = 0;
+            for (int i = 1; i <= max; i++)
+            {
+                if (i % 2 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        // implement this method so it returns true if input is "password"
+        // regardless of case
+        public static bool CheckInput(string input)
+        {
+            var CheckInput = input.ToLower() == "password" ? true : false;
+            return CheckInput;
+        }
+
+        // implement this method so it returns the sum
+        // of all the numbers in the list
+        public static int SumList(List<int> list)
+        {
+            var sum = 0;
+           foreach (int num in list)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
