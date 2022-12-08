a6b9cc4ad44ddc20171dc7bb67aca09a9564280c
Mehdi-Hamdi <mhamdi@spartaglobal.com>
2022-12-08 17:35:08 +0000

    added exception for getgreeting

--- Week 2/UnitTestLesson/CodeToTest/Program.cs
+++ Week 2/UnitTestLesson/CodeToTest/Program.cs
@@ -1,27 +1,37 @@
-﻿namespace CodeToTest;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CodeToTest;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        int timeOfDay = 21;
+        int timeOfDay = 33;
         Console.WriteLine(GetGreeting(timeOfDay));
     }
 
     public static string GetGreeting(int timeOfDay)
-    {
+    { 
+        
+       
         string message;
-        if (timeOfDay >= 5 && timeOfDay <= 12)
+        if (timeOfDay < 0 || timeOfDay > 23) 
         {
-            message = "Good morning!";
+            throw new ArgumentOutOfRangeException("time cannot be out of the range 0-23");
+        }
+        
+
+        if (timeOfDay >= 5 && timeOfDay < 12)
+        {
+            message = "Good Morning!";
         }
         else if (timeOfDay >= 12 && timeOfDay <= 18)
         {
-            message = "Good afternoon!";
+            message = "Good Afternoon!";
         }
         else
         {
-            message = "Good evening!";
+            message = "Good Evening!";
         }
         return message;
     }
--- Week 2/UnitTestLesson/UnitTests/UnitTest.cs
+++ Week 2/UnitTestLesson/UnitTests/UnitTest.cs
@@ -4,15 +4,120 @@ namespace UnitTests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+
+
+        [Test]
+        public void GivenTimeOf5_GetGreeting_ReturnsGoodMorning()
         {
+            //given a time of 5
+            //Arrange our precondition
+            int time = 5;
+
+            //when i call GetGreeting
+            //Act
+            string output = Program.GetGreeting(time);
+
+            //Assert that it returned "Good Morning!"
+            Assert.That(output, Is.EqualTo("Good Morning!"));
         }
 
         [Test]
-        public void Test1()
+        public void GivenTimeOf13_GetGreeting_ReturnsGoodAfternoon()
         {
-            Assert.Pass();
+            //given a time of 13
+            //Arrange our precondition
+            int time = 13;
+
+            //when i call GetGreeting
+            //Act
+            string output = Program.GetGreeting(time);
+
+            //Assert that it returned "Good Afternoon!"
+            Assert.That(output, Is.EqualTo("Good Afternoon!"));
+        }
+
+        [Test]
+        public void GivenTimeOf19_GetGreeting_ReturnsGoodEvening()
+        {
+            //given a time of 19
+            //Arrange our precondition
+            int time = 19;
+
+            //when i call GetGreeting
+            //Act
+            string output = Program.GetGreeting(time);
+
+            //Assert that it returned "Good Evening!"
+            Assert.That(output, Is.EqualTo("Good Evening!"));
+        }
+
+        [TestCase(5)]
+        [TestCase(8)]
+        [TestCase(11)]
+        public void GivenMorningBoundaryValues_GetGreeting_ReturnsGoodMorning(int timeOfDay)
+        {
+            //given a time of timeOfDay
+            //Arrange our precondition
+            int time = timeOfDay;
+
+            //when i call GetGreeting
+            //Act
+            string output = Program.GetGreeting(time);
+
+            //Assert that it returned "Good Morning!"
+            Assert.That(output, Is.EqualTo("Good Morning!"));
+        }
+
+        [TestCase(12)]
+        [TestCase(15)]
+        [TestCase(18)]
+        public void GivenAfternoonBoundaryValues_GetGreeting_ReturnsGoodAfternoon(int timeOfDay)
+        {
+            //given a time of timeOfDay
+            //Arrange our precondition
+            int time = timeOfDay;
+
+            //when i call GetGreeting
+            //Act
+            string output = Program.GetGreeting(time);
+
+            //Assert that it returned "Good Afternoon!"
+            Assert.That(output, Is.EqualTo("Good Afternoon!"));
+        }
+
+        [TestCase(19)]
+        [TestCase(21)]
+        [TestCase(23)]
+        public void GivenEveningBoundaryValues_GetGreeting_ReturnsGoodEvening(int timeOfDay)
+        {
+            //given a time of timeOfDay
+            //Arrange our precondition
+            int time = timeOfDay;
+
+            //when i call GetGreeting
+            //Act
+            string output = Program.GetGreeting(time);
+
+            //Assert that it returned "Good Evening!"
+            Assert.That(output, Is.EqualTo("Good Evening!"));
+        }
+
+        [TestCase(0)]
+        [TestCase(2)]
+        [TestCase(4)]
+        public void GivenEarlyMorningBoundaryValues_GetGreeting_ReturnsGoodEvening(int timeOfDay)
+        {
+            //given a time of timeOfDay
+            //Arrange our precondition
+            int time = timeOfDay;
+
+            //when i call GetGreeting
+            //Act
+            string output = Program.GetGreeting(time);
+
+            //Assert that it returned "Good Evening!"
+            Assert.That(output, Is.EqualTo("Good Evening!"));
         }
     }
+    
 }
