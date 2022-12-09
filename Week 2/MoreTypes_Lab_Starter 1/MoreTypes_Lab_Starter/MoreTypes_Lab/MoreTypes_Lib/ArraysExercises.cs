0ce044acb6a5037b7b93a8062cd8e2b9af3f9f46
Mehdi-Hamdi <mhamdi@spartaglobal.com>
2022-12-09 14:26:30 +0000

    array exercises

--- Week 2/MoreTypes_Lab_Starter 1/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
+++ Week 2/MoreTypes_Lab_Starter 1/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/ArraysExercises.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MoreTypes_Lib
 {
@@ -8,19 +9,93 @@ namespace MoreTypes_Lib
         // returns a 1D array containing the contents of a given List
         public static string[] Make1DArray(List<string> contents)
         {
-            throw new NotImplementedException();
+            string[] output = new string[contents.Count];
+            for (int i = 0; i < contents.Count; i++)
+            {
+                output[i] = contents[i];
+            }
+
+
+            return output;
+            
         }
 
         // returns a 3D array containing the contents of a given List
         public static string[,,] Make3DArray(int length1, int length2, int length3, List<string> contents)
         {
-            throw new NotImplementedException();
+            if (length1 + length2 + length3 != contents.Count)
+            {
+                throw new ArgumentException("Number of elements in list must match array size");
+            }
+            string[,,] output = new string[length1, length2, length3];
+            int index = 0;
+            for (int x = 0; x < length1; x++)
+            {
+                for (int y = 0; y < length2; y++)
+                {
+                    for (int z = 0; z < length3; z++)
+                    {
+                        output[x, y, z] = contents[index++];
+                    }
+                }
+            }
+            return output;
         }
+
+
+
+
+
+
+
+        //    if (length1*length2*length3 == contents.Count)
+        //    {
+        //        string[,,] stringArray = new string[length1, length2, length3];
+        //        stringArray[0, 0, 0] = "Alpha";
+        //        stringArray[0, 0, 1] = "Beta";
+        //        stringArray[0, 0, 2] = "Gamma";
+        //        stringArray[1, 0, 0] = "Delta";
+        //        stringArray[1, 0, 1] = "Epsilon";
+        //        stringArray[1, 0, 2] = "Zeta";
+        //        return stringArray;
+
+        //    }
+        //    else
+        //    {
+        //        throw new ArgumentException("Number of elements in list must match array size");
+        //    }
+        //    throw new NotImplementedException();
+        //}
+
+
+
+
 
         // returns a jagged array containing the contents of a given List
         public static string[][] MakeJagged2DArray(int countRow1, int countRow2, List<string> contents)
         {
-            throw new NotImplementedException();
+            if (countRow1 + countRow2 != contents.Count)
+            {
+                throw new ArgumentException("Number of elements in list must match array size");
+            }
+            string[][] output = new string[2][];
+            output[0] = new string[countRow1];
+            output[1] = new string[countRow2];
+            int index = 0;
+            foreach (string element in contents)
+            {
+                if (index < countRow1)
+                {
+                    output[0][index] = element;
+                }
+                else
+                {
+                    output[1][index-countRow1] = element;
+                }
+                index++;
+            }
+            return output;
         }
+            
     }
 }
--- Week 2/MoreTypes_Lab_Starter 1/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/StringExercises.cs
+++ Week 2/MoreTypes_Lab_Starter 1/MoreTypes_Lab_Starter/MoreTypes_Lab/MoreTypes_Lib/StringExercises.cs
@@ -22,26 +22,67 @@ namespace MoreTypes_Lib
         public static string Address(int number, string street, string city, string postcode)
         {
             return $"{Convert.ToString(number)} {street}, {city} {postcode}.";
-
             throw new NotImplementedException();
         }
+
         // returns a string representing a test score, written as percentage to 1 decimal place
         public static string Scorer(int score, int outOf)
         {
+            return $"You got {score} out of {outOf}: {100 * Math.Round((Convert.ToDouble(score) / Convert.ToDouble(outOf)), 1)}%";
             throw new NotImplementedException();
         }
 
         // returns the double represented by the string, or -999 if conversion is not possible
         public static double ParseNum(string numString)
         {
-            throw new NotImplementedException();
+            try
+            {
+                double output = Double.Parse(numString);
+                return output;
+            }
+            catch (FormatException)
+            {
+                return -999;
+            }
         }
 
-        // Returns the a string containing the count of As, Bs, Cs and Ds in the parameter string
+        // Returns a string containing the count of As, Bs, Cs and Ds in the parameter string
         // all other letters are ignored
         public static string CountLetters(string input)
         {
-            throw new NotImplementedException();
+
+            int A = 0;
+            int B = 0;
+            int C = 0;
+            int D = 0;
+
+            if (input.Length > 0)
+            {
+                foreach (char c in input)
+                {
+                    if (c == 'A')
+                    {
+                        A++;
+                    }
+                    else if (c == 'B')
+                    {
+                        B++;
+                    }
+                    else if (c == 'C')
+                    {
+                        C++;
+                    }
+                    else if (c == 'D')
+                    {
+                        D++;
+                    }
+                }
+                return $"A:{A} B:{B} C:{C} D:{D}";
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
