d56f8acecbbc850aacd048ad5650b4abe14960e3
Mehdi-Hamdi <mhamdi@spartaglobal.com>
2022-12-09 12:05:15 +0000

    more datatypes continued

--- Week 2/DataTypesAndMethods/MoreDataTypesApp/Program.cs
+++ Week 2/DataTypesAndMethods/MoreDataTypesApp/Program.cs
@@ -9,7 +9,10 @@ namespace MoreDataTypesApp
             var myString = " C# list fundamentals";
             Console.WriteLine(StringBuilderExercise(myString));
             StringInterporlation("Mehdi");
-            ParsingStrings();
+            //ParsingStrings();
+            OneDArrays();
+            MultiDArrays();
+            JaggedArrays();
         }
 
         public static string StringBuilderExercise(string myString)
@@ -41,5 +44,76 @@ namespace MoreDataTypesApp
             var success = Int32.TryParse(input, out int parsedApples);
         }
 
+        public static void OneDArrays()
+        {
+            int[] myIntArray = { 12, 23, 34, 45, 56 };
+            // Array.Reverse(myIntArray); can use this method as well
+            foreach (var element in myIntArray.Reverse())
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        public static void MultiDArrays()
+        {
+            //2D Arrays
+            int[,] grid = new int[2, 4];
+            grid[0, 1] = 6;
+            grid[1, 0] = 8;
+            grid[1, 3] = 10;
+
+            char[,] gridTwo =
+            {
+            { 'a', 'b' },
+            { 'a', 'b' },
+            { 'a', 'b' },
+            { 'a', 'b' },
+            { 'a', 'b' },
+           };
+            Console.WriteLine("All of grid two");
+            foreach (var element in gridTwo)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("GridTwo row by row");
+            for (int i = gridTwo.GetLowerBound(0); i <= gridTwo.GetUpperBound(0); i++)
+            {
+                for (int j = gridTwo.GetLowerBound(1); j <= gridTwo.GetUpperBound(1); j++)
+                {
+                    Console.WriteLine($"({i}, {j}) {gridTwo[i, j]} ");
+                }
+
+            }
+        }
+
+        public static void JaggedArrays()
+        {
+            int[][] intJArray = new int[2][];
+            intJArray[0] = new int[4];
+            intJArray[1] = new int[2];
+
+            intJArray[0][2] = 3;
+            intJArray[1][0] = 5;
+
+            string[][] animalJArray = new string[][]
+            {
+                new string[] { "Hyena", "lion", "cheetah", "Panther" },
+                new string[] { "peacock", "canary" }
+            };
+
+            Console.WriteLine("Jagged array of animals");
+            foreach (var element in animalJArray)
+            {
+                foreach (var animal in element)
+                {
+                    Console.WriteLine(animal);
+                }
+            }
+               
+
+
+        }
+           
+        
     }
 }
